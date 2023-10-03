f0dcdae2c0308c3a1637a3c6d1dab0534bcbfea9
Diego Alonso Avendaño Cardenas <111781924+DiegoAvendanoCardenas@users.noreply.github.com>
2023-10-03 09:14:15 -0500

    ejercicios

--- 03_DiegoAvendano/src/S07_03DiegoAvendano/Main_S07_03DiegoAvendano.java
+++ 03_DiegoAvendano/src/S07_03DiegoAvendano/Main_S07_03DiegoAvendano.java
@@ -4,13 +4,13 @@ package S07_03DiegoAvendano;
 
 public class Main_S07_03DiegoAvendano {
     public static void main(String[] args) {
-        persona1 objPer1 = new persona1();
+        persona1_03 objPer1 = new persona1_03();
         
         
         objPer1.correr(10);
         objPer1.persona(" Diego", 18);
         
-        persona2 objPer2 = new persona2();
+        persona2_03 objPer2 = new persona2_03();
         
         objPer2.año(2023);
         objPer2.altura(1.66);
