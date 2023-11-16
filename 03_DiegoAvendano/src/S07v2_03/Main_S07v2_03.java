5f96ca1cdac4c8cff35e4d060cbd4c3f4c7ddd5b
Diego Alonso Avendaño Cardenas <111781924+DiegoAvendanoCardenas@users.noreply.github.com>
2023-11-15 22:49:15 -0500

    Ejercicio S07_03

--- 03_DiegoAvendano/src/S07v2_03/Main_S07v2_03.java
+++ 03_DiegoAvendano/src/S07v2_03/Main_S07v2_03.java
@@ -3,11 +3,13 @@ package S07v2_03;
 
 public class Main_S07v2_03 {
     public static void main(String[] args) {
+        //Objeto 03_DiegoAvendaño
         persona_03 objPersona = new persona_03();
         
-        
-        objPersona.frase();
+        // Argumentos 03_DiegoAvendaño y Objetos de la clase Persona_03
+        objPersona.frase("Me gusta ser competitivo");
         objPersona.numeroDeHermanos(5);
+        
         
     }
     
--- 03_DiegoAvendano/src/S07v2_03/persona_03.java
+++ 03_DiegoAvendano/src/S07v2_03/persona_03.java
@@ -3,21 +3,27 @@ package S07v2_03;
 
 
 public class persona_03 {
-    //Atributos
+    //Atributos 03_DiegoAvendaño
     String nombre;
     int edad;
     String sexo;
     double altura;
     
     
-    //Metodos
+    //Metodos 03_DiegoAvendaño
+    public void aprender(){
+        System.out.println("Aprender nuevas cosas");
+    }
+    
     public void correr(){
         System.out.println("salgo en la mañana a correr");
     }
     public void comer(){
         System.out.println("me gusta comer");
     }
-    //SobreCarga de metodos
+    
+    
+    //SobreCarga de metodos 03_DiegoAvendaño
     public void metodos1 (){
         
     }
@@ -29,9 +35,9 @@ public class persona_03 {
     }
     
     
-    //Metodos con valores de retorno
-    public String frase(){
-        System.out.println("soy el mejor :v");
+    //Metodos con valores de retorno 03_DiegoAvendaño
+    public String frase(String saludo){
+        System.out.println(saludo);
         return "";
     }
     
@@ -44,10 +50,10 @@ public class persona_03 {
         return 0.0;
     }
     
-    //Metodos con parametros y argumentos
+    //Metodos con parametros y argumentos 03_DiegoAvendaño
     
     public void hola(int a , int b){
-     
+        
     }
     
     public void caminar(String a , String b){
